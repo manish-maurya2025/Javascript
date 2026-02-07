d921736ab0e071a0d19e6203a298c9e097a45f57
manish-maurya2025 <manishmaurya6665@gmail.com>
2026-02-07 22:13:15 +0530

    first commit

--- test.js
+++ test.js
@@ -1,11 +1,12 @@
-const accountId = "123= "manish@google.com"
-var accountPas456"
-let accountEmail sword = "1234"
-accountCity = "Lucknow"
+const accountId = "123";
+let accountEmail = "manish@google.com";
+var accountPassword = "456";
+let accountCity = "Lucknow";
 let accountState;
-accountEmail = "mkm@gmail.com"
-accountPassword = "6586"
-accountCity = "Noida"
+
+accountEmail = "mkm@gmail.com";
+accountPassword = "6586";
+accountCity = "Noida";
 
 console.log(accountId);
-console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState]);
