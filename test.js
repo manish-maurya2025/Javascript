c449bf1c8ae8e89d458a95630a0cd57265402fb9
manish-maurya2025 <manishmaurya6665@gmail.com>
2026-02-07 21:39:42 +0530

    BasicJs

---
+++ test.js
@@ -0,0 +1,11 @@
+const accountId = "123456"
+let accountEmail = "manish@google.com"
+var accountPassword = "1234"
+accountCity = "Lucknow"
+let accountState;
+accountEmail = "mkm@gmail.com"
+accountPassword = "6586"
+accountCity = "Noida"
+
+console.log(accountId);
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
