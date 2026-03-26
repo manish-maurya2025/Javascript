f4494f95b0bbc15b2a828eb197bda02d33b42eb3
manish-maurya2025 <manishmaurya6665@gmail.com>
2026-03-26 23:16:07 +0530

    datatypein javascript

---
+++ basic_01/01_variable.js
@@ -0,0 +1,13 @@
+const accountId = "123";
+let accountEmail = "manish@google.com";
+var accountPassword = "456";
+let accountCity = "Lucknow";
+let accountState;
+
+accountEmail = "mkm@gmail.com";
+accountPassword = "6586";
+accountCity = "Noida";
+
+console.log(accountId);
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState]);
+
---
+++ basic_01/02_datatype.js
@@ -0,0 +1,12 @@
+"use strict"; 
+// treat all JS code as newer verser
+console.log(3+3)
+console.log("manish")
+let name = "manish"
+let age = 21
+let isLoggedIn = false 
+let state;
+console.log(typeof "manish")
+console.log(typeof age)
+console.log(typeof null)
+console.log(typeof nudefined)
