364415c6aa48a16cfa310e6d637994ce11057d1f
manish-maurya2025 <manishmaurya6665@gmail.com>
2026-03-27 22:14:25 +0530

    Operations in datatype

--- basic_01/03_conversionOperation.js
+++ basic_01/03_conversionOperation.js
@@ -1,23 +1,46 @@
-let score = "manish"
+// let score = "manish"
 
-console.log(typeof score);
-console.log(typeof(score));
+// console.log(typeof score);
+// console.log(typeof(score));
 
-let valueInNumber = Number(score)
-console.log (typeof valueInNumber);
-console.log(valueInNumber); 
+// let valueInNumber = Number(score)
+// console.log (typeof valueInNumber);
+// console.log(valueInNumber); 
 // "33" => 33
 // "33abc" => NaN
 //  tre => 1; false => 0
-let isLoggedIn = "manish"
-let booleanIsLoggedIn = Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn)
+// let isLoggedIn = "manish"
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn)
 
 // 1 => true; 0 => false
 // "" => false
 // "manish" => true
 
-let someNumber = 33
-let stringNumber = String(someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber)
+// let someNumber = 33
+// let stringNumber = String(someNumber)
+// console.log(stringNumber);
+// console.log(typeof stringNumber)
+
+// *************Operations ************
+
+let value = 3
+let negValue = -value
+// console.log(negValue);
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**2);
+// console.log(2/3);
+// console.log(2%3);
+
+let str1 = "manish"
+let str2 = " maurya"
+let str3 = str1  +  str2
+
+console.log(str3);
+
+console.log("1" + 2);
+console.log(1 + "2");
+console.log("1" + 2 + 2);
+console.log(1 + 2 + "2");
