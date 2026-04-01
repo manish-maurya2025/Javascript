d187e4072951c413578e5bb71c81073c521af556
manish-maurya2025 <manishmaurya6665@gmail.com>
2026-04-01 20:55:04 +0530

    dateinjavascript

---
+++ basic_01/07_dateinjs.js
@@ -0,0 +1,36 @@
+// dates
+
+let myDate = new Date()
+// console.log(myDate.toString());
+// console.log(myDate.toDateString());
+// console.log(myDate.toISOString());
+// console.log(myDate.toJSON());
+// console.log(myDate.toLocaleDateString());
+// console.log(myDate.toLocaleTimeString());
+// console.log(myDate.toTimeString());
+// console.log(myDate.toUTCString());
+// console.log(myDate.getTimezoneOffset());
+// console.log(typeof myDate);
+
+// let myCreatedDate = new Date(2026, 3, 1)
+
+// let myCreatedDate = new Date(2026, 3, 1, 5, 3)
+let myCreatedDate = new Date("01-04-2026")
+
+// console.log(myCreatedDate.toLocaleString());
+
+let  myTimeStamp = Date.now()
+
+// console.log(myTimeStamp);
+// console.log(myCreatedDate.getTime());
+// console.log(Math.floor(Date.now()/1000));
+
+let newDate = new Date()
+console.log(newDate);
+console.log(newDate.getMonth() + 1);
+console.log(newDate.getDay());
+
+newDate.toLocaleString('default', { 
+    weekday: "long"
+})
+console.log(newDate.toLocaleString());
