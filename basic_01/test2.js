337920de282959ac8f19923a7a47cd4803095441
manish-maurya2025 <manishmaurya6665@gmail.com>
2026-04-02 20:56:20 +0530

    test2

---
+++ basic_01/test2.js
@@ -0,0 +1,16 @@
+// console.log("Start");
+
+// setTimeout(() => {
+//     console.log("Timeout");
+// }, 0);
+
+// console.log("End");
+const accountId= "129402";
+accountEmail = "manish@gmail.com";
+accountPassword = "manish@123";
+accountState = "UP";
+accountPhoneNumber = "6386390665";
+console.log(accountId);
+console.table([accountId,accountEmail,accountPassword,accountstate,accountPhoneNumber]);
+
+
